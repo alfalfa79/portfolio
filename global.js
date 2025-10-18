9371de0b78e37ed648ef4c8f38d74d13b5626c15
Odessa Castillo <odessacastillo8@gmail.com>
2025-10-17 23:23:09 -0700

    10/17 updates

---
+++ global.js
@@ -0,0 +1,84 @@
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+let navLinks = $$("nav a")
+
+// let currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname,
+// );
+
+// currentLink.classList.add('current');
+
+// if (currentLink) {
+//   // or if (currentLink !== undefined)
+//   currentLink?.classList.add('current');
+// }
+
+const BASE_PATH =
+  (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+    ? "/"
+    : "/portfolio/";
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'resume/', title: 'Resume' }
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url.startsWith('http') ? p.url : BASE_PATH + p.url;
+  let a = document.createElement('a');
+    a.href = url;
+    a.textContent = p.title;
+    nav.append(a);
+
+    a.classList.toggle('current',
+    a.host === location.host && a.pathname === location.pathname,
+    );
+    if(a.host != location.host) a.target = "_blank"
+
+}
+
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+	<label class="color-scheme">
+		Theme:
+		<select>
+			<option value="light dark">Automatic</option>
+            <option value="light">Light</option>
+            <option value="dark">Dark</option>
+		</select>
+	</label>`,
+);
+
+matchMedia("(prefers-color-scheme: dark)").matches;
+
+const select = document.querySelector('.color-scheme select');
+
+ if ("colorScheme" in localStorage) {
+    const savedScheme = localStorage.colorScheme;
+     document.documentElement.style.setProperty("color-scheme", savedScheme);
+     select.value = savedScheme;
+  }
+
+select.addEventListener('input', function (event) {
+  console.log('color scheme changed to', event.target.value);
+  localStorage.colorScheme = event.target.value;
+  document.documentElement.style.setProperty('color-scheme', event.target.value);
+ 
+  if ("colorScheme" in localStorage) {
+    const savedScheme = localStorage.colorScheme;
+     document.documentElement.style.setProperty("color-scheme", savedScheme);
+     select.value = savedScheme;
+  }
+});
+
+
